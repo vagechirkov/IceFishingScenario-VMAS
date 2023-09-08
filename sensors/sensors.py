82f23fef8ab1bc4700e40155d0d9a9ebc17c1db9
valery <vagechirkov@gmail.com>
2023-09-08 11:48:11 +0200

    fix sensors output format

--- sensors/sensors.py
+++ sensors/sensors.py
@@ -11,13 +11,13 @@ class AgentDetector(Sensor):
     def measure(self):
         # iterate over all agents not including self
         m = torch.stack([
-            torch.stack([agent.state.pos, agent.state.vel], dim=0)
+            torch.stack([agent.state.pos, agent.state.vel], dim=-1).flatten(1)
             for agent in self._world.agents
             if agent is not self.agent
-        ])
+        ], dim=-1)
 
         # flatten and convert to tensor
-        return m.flatten().unsqueeze(0)
+        return m.flatten(1)
 
     def render(self, env_index: int = 0):
         return []
